7a095a7815d6e6bd52cb150c18d900c0d8626b10
dattaprasadthakur <dattaprasad.s.thakur@gmail.com>
2026-07-08 16:38:19 -0400

    testing commit

---
+++ ThreadCreator.java
@@ -0,0 +1,14 @@
+public class ThreadCreator implements Runnable{
+
+    @Override
+    public void run(){
+        System.out.println("Hello world from run");
+    }
+
+    public static void main(String[] s){
+        //way to spawn a thread in jdk 25
+        Runnable r = new ThreadCreator();
+        Thread.ofPlatform().name("Test-Thread").daemon(false).start(r);
+    }
+
+}
