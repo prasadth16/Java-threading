1be38a7531286bca5e7975c3e26c641b852e9e33
dattaprasadthakur <dattaprasad.s.thakur@gmail.com>
2026-07-15 22:44:24 -0400

    commiting new class

---
+++ RecordDemo.java
@@ -0,0 +1,26 @@
+import java.time.LocalDate;
+import java.time.Month;
+
+public class RecordDemo {
+    public static void main(String[] args) {
+        Person prasad1 = new Person("Prasad", "Thakur", LocalDate.of(1980, Month.JULY, 16), "880 Powder Mill Road, Wilmington DE 19803");
+        Person prasad2 = new Person("Prasad", "Thakur", LocalDate.of(1980, Month.JULY, 16), "880 Powder Mill Road, Wilmington DE 19803");
+        //Person prasad3 = new Person(null, "Thakur", LocalDate.of(1980, Month.JULY, 16), "880 Powder Mill Road, Wilmington DE 19803");
+
+        System.out.println("Are they equal? " + (prasad1.equals(prasad2)));
+        Person.getFullName(prasad1);
+    }
+
+    record Person(String firstName, String lastName, LocalDate birthDate, String address) {
+        public Person {
+            if (firstName == null || lastName == null || birthDate == null || address == null) {
+                throw new IllegalArgumentException("All fields must be non-null");
+            }
+        }
+
+        public static void getFullName(Person persn) {
+            System.out.println(persn.firstName() + " " + persn.lastName());
+        }
+
+    }
+}
